3aafea3c80becbbdfbaee41304d965a7bd754d26
adilrasheed139 <160167319+adilrasheed139@users.noreply.github.com>
2024-03-30 17:39:42 +0500

    Update and rename Pie Chart.Rmd to Pie Chart.r

--- Pie Chart.r
+++ Pie Chart.r
@@ -1,113 +1,64 @@
----
-title: "Pie Chart"
-author: "Adil Rasheed"
-date: "2024-03-26"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-### Pie Chart for Categorical Data
-
-```{r}
+# Pie Chart for Categorical Data
 carb.count <- table(mtcars$carb)
 carb.count
-```
-### Simple Pie Chart with Defult Arguments Values
 
-```{r}
+# Simple Pie Chart with Defult Arguments Values
 pie(carb.count)
-```
 
-### Show the pie chart on Clockwise
-
-```{r}
 # Show the pie chart on Clockwise
 pie(carb.count, clockwise = T)
-```
 
-### Simple Pie Chart with Labels
-
-```{r}
+# Simple Pie Chart with Labels
 pie (carb.count, labels = carb.count)
-```
 
-### Pie Chat with Custimized lables names
-
-```{r}
+# Pie Chat with Customized lables names
 pie (carb.count, labels = c("one", "two", "three", "four", "five", "six"))
-```
 
-### Pie Chat with Custimized lables names and increase the label size
-
-```{r}
+# Pie Chat with Customized labels names and increase the label size
 pie (carb.count, labels = c("one", "two", "three", "four", "five", "six"), 
      cex = 2) # CEX increase the size of labels
-```
 
-### Pie Chat and adding customized colors
-
-```{r}
+# Pie Chat and adding customized colors
 pie (carb.count, labels = c("one", "two", "three", "four", "five", "six"), 
      col = c("red", "blue", "green", "yellow", "black","grey"))
-```
 
-### Pie Chat and style of all border
-
-```{r}
+# Pie Chat and style of all border
 pie (carb.count, labels = c("one", "two", "three", "four", "five", "six"), 
      col = c("red", "blue", "green", "yellow", "black","grey"),
              lty = 2)
-```
 
-### Pie Chat and add the density
-
-```{r}
+# Pie Chat and add the density
 pie (carb.count, labels = c("one", "two", "three", "four", "five", "six"), 
      col = c("red", "blue", "green", "yellow", "black","grey"),
              lty = 2,
              density= 50)
-```
 
-### Pie Chat with different angle
-
-```{r}
+# Pie Chat with a different angle
 pie (carb.count, labels = c("one", "two", "three", "four", "five", "six"), 
      col = c("red", "blue", "green", "yellow", "black","grey"),
              lty = 2,
              density= 50,
      angle =90)
-```
 
-### Pie Chat and style the edges
-
-```{r}
+# Pie Chat and style the edges
 pie (carb.count, labels = c("one", "two", "three", "four", "five", "six"), 
      col = c("red", "blue", "green", "yellow", "black","grey"),
              lty = 2,
              density= 50,
      angle =90,
      edges = 10)
-```
 
-### Pie Chart with Legends
-
-```{r}
+# Pie Chart with Legends
 pie (carb.count, labels = c("A", "B", "C", "D", "E", "F"), 
      col = rainbow (6)
      )
 legend("topright", legend = c("A", "B", "C", "D", "E", "F"),
        fill = rainbow(6), title = "Legend"
        )
-```
 
-### 3D Pie Chart
-
-```{r}
-### pdf(filename.pdf)
-## devoff()
+# 3D Pie Chart
+# pdf(filename.pdf)
+# devoff()
 pie3D(carb.count,
     col = rainbow(6),
     height = 0.2, # Height of the Pie Chart
@@ -116,4 +67,3 @@ pie3D(carb.count,
     explode = 0.25, # Used for create looks cake shape
     radius = 1.5, # used to expand the pie chart
     )
-```
